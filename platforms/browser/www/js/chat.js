61b75a25b39ee851edad795dde1923c2876937f8
chris stewart <gameboydrewy@gmail.com>
2018-04-04 18:57:23 +0530

    first

--- platforms/browser/www/js/chat.js
+++ platforms/browser/www/js/chat.js
@@ -1,18 +1,27 @@
  window.onload = function() {
   document.getElementById("myText").focus();
   };
-  function typo(){
+  function typo() {
+  if (document.getElementById("myText").value.trim().length == 0) {
+     document.getElementById("myText").focus();
+    
+  } else {
     var currentText = document.getElementById("demo").innerHTML;
     var x = '<div><p class=bubble>' + document.getElementById("myText").value + '</p></div>';
     document.getElementById("myText").value = "";
     var y = document.getElementById("demo").innerHTML = currentText + x;
-     var z = document.getElementById('demo');
-  z.scrollTop = z.scrollHeight;
+
+    var z = document.getElementById('demo');
+    z.scrollTop = z.scrollHeight;
+    document.getElementById("myText").focus();  
   }
+}
+  
   var input = document.getElementById("myText");
   input.addEventListener("keyup", function(event) {
     event.preventDefault();
-  if (event.keyCode === 13 && input.value.length >=1) {
+  if (event.keyCode === 13 && input.value.trim().length >0) {
       document.getElementById("btn-chat").click();
+
     }
   });
