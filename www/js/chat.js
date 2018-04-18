ea96fe3fc52752dfef89c88c5839139e745ecb39
chris stewart <gameboydrewy@gmail.com>
2018-04-18 17:57:03 +0530

    win

--- www/js/chat.js
+++ www/js/chat.js
@@ -11,36 +11,12 @@
     document.getElementById("myText").value = "";
     var y = document.getElementById("demo").innerHTML = currentText + x;
 
-    var z = document.getElementById('demo');
+    var z = document.getElementById('wrapper');
     z.scrollTop = z.scrollHeight;
     document.getElementById("myText").focus();  
   }
 }
-   function isTouchDevice(){
-    try{
-        document.createEvent("TouchEvent");
-        return true;
-    }catch(e){
-        return false;
-    }
-}
-
-function touchScroll(id){
-    if(isTouchDevice()){ //if touch events exist...
-        var el=document.getElementById(id);
-        var scrollStartPos=0;
-
-        document.getElementById(id).addEventListener("touchstart", function(event) {
-            scrollStartPos=this.scrollTop+event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-
-        document.getElementById(id).addEventListener("touchmove", function(event) {
-            this.scrollTop=scrollStartPos-event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-}
-}
+   
   var input = document.getElementById("myText");
   input.addEventListener("keyup", function(event) {
     event.preventDefault();
