6a258d2272b2d2ec7275930a3ba7e838bab98eb3
chris stewart <gameboydrewy@gmail.com>
2018-04-19 12:31:51 +0530

    3

--- www/js/chat.js
+++ www/js/chat.js
@@ -16,31 +16,7 @@
     document.getElementById("myText").focus();  
   }
 }
-function isTouchDevice(){
-    try{
-        document.createEvent("TouchEvent");
-        return true;
-    }catch(e){
-        return false;
-    }
-}
 
-function touchScroll(id){
-    if(isTouchDevice()){ //if touch events exist...
-        var el=document.getElementById(id);
-        var scrollStartPos=0;
-
-        document.getElementById(id).addEventListener("touchstart", function(event) {
-            scrollStartPos=this.scrollTop+event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-
-        document.getElementById(id).addEventListener("touchmove", function(event) {
-            this.scrollTop=scrollStartPos-event.touches[0].pageY;
-            event.preventDefault();
-        },false);
-}
-} 
   
   var input = document.getElementById("myText");
   input.addEventListener("keyup", function(event) {
