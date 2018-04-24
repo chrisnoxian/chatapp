edb16cda060933600a6c3e7c02b410937ded5d39
chris stewart <gameboydrewy@gmail.com>
2018-04-24 12:24:00 +0530

    keypad1

--- www/js/chat.js
+++ www/js/chat.js
@@ -20,6 +20,47 @@
   }
 }
 
+ //when keypad is open
+  window.addEventListener('native.keyboardshow', function (e) {
+    var deviceHeight = window.innerHeight;
+    var keyboardHeight = e.keyboardHeight;
+    var deviceHeightAdjusted = deviceHeight - keyboardHeight;//device height adjusted
+    deviceHeightAdjusted = deviceHeightAdjusted < 0 ? (deviceHeightAdjusted * -1) : deviceHeightAdjusted;//only positive number
+    document.getElementById('page').style.height = deviceHeightAdjusted + 'px';//set page height
+    document.getElementById('page').setAttribute('keyBoardHeight', keyboardHeight);//save keyboard height
+});
+
+  //when keypad is closed
+  window.addEventListener('native.keyboardhide', function (e) {
+    setTimeout(function () {
+        document.getElementById('page').style.height = 100 + '%';//device  100% height
+    }, 100);
+    var inputs = document.querySelectorAll('input');//select all input
+var n = inputs.length;
+for (var i = 0; i < n; i++) {
+    var input = inputs[i];
+    input.addEventListener('focus', function (e) {//add event focus
+        var inp = this; 
+       //wait 0.6 seconds to scroll down to the input has focus
+        setTimeout(function () {
+            try {
+                //if the keyboard is open
+                if (cordova.plugins.Keyboard.isVisible) {
+                    var padding = 15;
+                    var targetPosition = parseInt($$(inp).offset().top + padding);
+                    var keyboardHeight = parseInt(document.getElementById('page').getAttribute('keyBoardHeight'));//get keyboard height   
+
+                    //if the input is hidden by the keyboard,scroll to the input 
+                    if (targetPosition >= keyboardHeight) {
+                        padding *=5;
+                        document.getElementById('page').scrollTop = targetPosition - padding;
+                    }
+                }
+            } catch (ex) {
+                alert(ex.message);
+            }
+        }, 600);
+    }, true);
   
    var input = document.getElementById("myText");
  $("textarea").keypress(function(e){
@@ -38,21 +79,3 @@
     
 }
  
- document.addEventListener('deviceready', 
-  function(){
-    // disable immersive mode  on Android when keyboard is shown
-        try {
-      if (cordova.platformId == 'android') {
-        AndroidFullScreen.immersiveMode(false, false);
-        window.addEventListener('native.keyboardshow', function (e) {
-          AndroidFullScreen.showSystemUI(false, false);
-
-        });
-        window.addEventListener('native.keyboardhide', function (e) {
-          AndroidFullScreen.immersiveMode(false, false);
-        });
-      }
-    } catch (error) {
-      console.log('deviceready - ' + error);
-    }
-}, false);
